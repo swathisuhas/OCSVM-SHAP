98ce04d556e4a686b6b43ee456c9c07aa17e6429
Swathi <swathisuhas46@gmail.com>
2025-04-26 17:55:14 +0200

    update

--- src/utils/visualisation/deterministic_values.py
+++ src/utils/visualisation/deterministic_values.py
@@ -1,5 +1,6 @@
 import shap
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 
 def local_bar_plot(shapley_values: torch.Tensor, index:int, **kwargs):
@@ -18,3 +19,65 @@ def summary_plot(shapley_values: torch.Tensor, **kwargs):
     plt.title("Beeswarm summary plot")
     plt.xlabel("SHAP value (impact on model output)")
     plt.show
+
+def summary_explanation_plot(shapley_values: torch.Tensor, feature_names, datasets, **kwargs):
+    if isinstance(shapley_values, torch.Tensor):
+        shapley_values = shapley_values.detach().cpu().numpy()
+
+    # Datasets is a list of groups: each group is a list of samples
+    group_data = [np.mean(group, axis=0) for group in datasets]
+    group_data = np.array(group_data)
+
+    explanation = shap.Explanation(
+        values=shapley_values,
+        feature_names=feature_names,
+        data = group_data
+    )
+
+    # Now using shap.plots.summary instead of shap.summary_plot
+    shap.plots.violin(explanation, show=False, **kwargs)
+
+    plt.title("Violin summary plot")
+    plt.xlabel("SHAP value (impact on model output)")
+    plt.show()
+
+def local_explanation_bar_plot(shapley_values: torch.Tensor, index:int, feature_names, datasets, **kwargs):
+    if isinstance(shapley_values, torch.Tensor):
+        shapley_values = shapley_values.detach().cpu().numpy()
+        
+    # Datasets is a list of groups: each group is a list of samples
+    group_data = [np.mean(group, axis=0) for group in datasets]
+    group_data = np.array(group_data)
+
+    # Build Explanation object for the selected index
+    explanation = shap.Explanation(
+        values=shapley_values[index],
+        data=group_data[index],
+        feature_names=feature_names
+    )
+
+    # Plot
+    title = f"SHAP value (Local Explanation) for index {index}"
+    shap.plots.bar(explanation, show=False, max_display=5)
+    plt.xlabel(title)
+    plt.tight_layout()
+    plt.show()
+
+
+def global_explanation_bar_plot(shapley_values: torch.Tensor, feature_names, datasets, **kwargs):
+    if isinstance(shapley_values, torch.Tensor):
+        shapley_values = shapley_values.detach().cpu().numpy()
+
+    # Mean across all groups
+    mean_data = np.mean([np.mean(group, axis=0) for group in datasets], axis=0)
+
+    explanation = shap.Explanation(
+        values=shapley_values,        
+        data=mean_data,                 
+        feature_names=feature_names
+    )
+
+    shap.plots.bar(explanation, show=False, max_display=5, **kwargs)
+    plt.xlabel("Absolute Mean SHAP value (Global Explanation)")
+    plt.tight_layout()
+    plt.show()
